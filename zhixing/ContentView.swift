3c03aa36ac86725ee2e3a486cffc15644586b096
cxf <1017416696@qq.com>
2024-09-07 16:44:21 +0800

    Initial Commit

---
+++ zhixing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  zhixing
+//
+//  Created by 曹骁凡 on 2024/9/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ zhixing/zhixingApp.swift
@@ -0,0 +1,17 @@
+//
+//  zhixingApp.swift
+//  zhixing
+//
+//  Created by 曹骁凡 on 2024/9/7.
+//
+
+import SwiftUI
+
+@main
+struct zhixingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
